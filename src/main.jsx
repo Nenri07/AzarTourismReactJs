571c07ef5535c35d13dc15d6b441956bc0d20298
Umer Bashir <128254092+Nenri07@users.noreply.github.com>
2026-02-06 23:05:52 +0500

    add toaster for toast messages

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
 import store from "./store/store";
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import usePersistLogin from "./hooks/usePersistLogin";
@@ -271,6 +272,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistWrapper>
+          <Toaster position="top-center" />
         <RouterProvider router={router} />
       </PersistWrapper>
     </Provider>
